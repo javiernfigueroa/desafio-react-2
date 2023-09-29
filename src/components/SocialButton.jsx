28db5f712e9b02450dfac793b8e218a0b887eef9
Ayxa Chaverra <achaverrar@gmail.com>
2023-09-28 20:17:28 -0500

    add borders to SocialButton components and refactor the corresponding code to more closely match the design

--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -1,7 +1,7 @@
-export function SocialButton(props) {
+export function SocialButton({icon}) {
   return (
-      <button className="btn btn-lg btn-light rounded-circle m-2">
-        {props.icon}
+      <button className="btn btn-lg btn-light rounded-circle m-2 border-black pt-1" style={{width: "3em", height: "3em"}}>
+        {icon}
       </button>
   );
 }
