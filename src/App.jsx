c91d398c87dd0b1c352b648c5be5090e5311a0b4
Ayxa Chaverra <achaverrar@gmail.com>
2023-09-28 19:54:29 -0500

    lift the alert related state up to the App component to match the requirements

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { Registration } from "./components/Registration";
 import './App.css';
 
 function App() {
+  const [alertMessage, setAlertMessage] = useState('');
+  const [alertColor, setAlertColor] = useState('');
   return (
     <>
-      <Registration />
+      <Registration
+        alertMessage={alertMessage}
+        setAlertMessage={setAlertMessage}
+        alertColor={alertColor}
+        setAlertColor={setAlertColor}
+      />
     </>
   );
 }
